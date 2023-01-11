b4c87b5df9ad7a60f9200ec9088e42e81341f984
toprasad <37651949+toprasad@users.noreply.github.com>
2023-01-11 13:05:29 +0530

    Update BasicLambda.java

--- lambdaExpression/src/com/lambdaexpression/sample/BasicLambda.java
+++ lambdaExpression/src/com/lambdaexpression/sample/BasicLambda.java
@@ -22,4 +22,16 @@ public class BasicLambda {
 		sortedEmployeeMap.entrySet().stream().forEach(System.out :: println);
 	}
 
+	
+// 	Map<String, Map<String, String>> test = resultMap.entrySet().stream()
+// 				.filter(e -> LocalDate.parse(e.getValue().get(SCASH_DATE_EFFECT)).compareTo(dateEffect) <= 0)
+// 				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+// 		test.entrySet().stream().forEach(System.out::println);
+// 		var tst2 = resultMap.entrySet().stream()
+// 				.filter(e -> LocalDate.parse(e.getValue().get(SCASH_DATE_EFFECT)).compareTo(dateEffect) <= 0)
+// 				.sorted((m1, m2) -> m2.getValue().get(SCASH_DATE_EFFECT)
+// 						.compareTo(m1.getValue().get(SCASH_DATE_EFFECT)))
+// 				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+// 		test.entrySet().stream().forEach(System.out::println);
 }
